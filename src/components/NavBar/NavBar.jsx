d1b24a6292202fadcdd60683c34547efbe52bf79
Metranohm <andrewwink@gmail.com>
2022-12-08 15:13:38 -0600

    update NavBar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,28 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Nav, Navbar } from 'react-bootstrap';
 
-const NavBar = () => (
-  <Navbar bg="light" expand="lg">
-    <Navbar.Brand as={Link} to="/">
-      My Website
-    </Navbar.Brand>
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="mr-auto">
-        <Nav.Link as={Link} to="/about">
-          About
-        </Nav.Link>
-        <Nav.Link as={Link} to="/products">
-          Products
-        </Nav.Link>
-        <Nav.Link as={Link} to="/contact">
-          Contact
-        </Nav.Link>
-      </Nav>
-    </Navbar.Collapse>
-  </Navbar>
-);
+const NavBar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
 
 export default NavBar;
 
