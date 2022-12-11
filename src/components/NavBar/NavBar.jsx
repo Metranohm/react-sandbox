2eb1ab998f0747dff84c71818ca136e686c0a86d
Metranohm <andrewwink@gmail.com>
2022-12-10 18:07:34 -0600

    implement contacts component

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,7 +3,8 @@ import { Link } from 'react-router-dom';
 
 const NavBar = () => {
   return (
-    <nav>
+    <nav className='navcontainer'>
+      <h1>NavBar</h1>  
       <ul>
         <li>
           <Link to="/">Home</Link>
